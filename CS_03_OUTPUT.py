154d6dba394ee79b36578d0a557f0aed696d3e76
2100039033 <137202274+2100039033@users.noreply.github.com>
2024-06-22 22:22:25 +0530

    Add files via upload

---
+++ CS_03_OUTPUT.py
@@ -0,0 +1,9 @@
+Enter a password to check its complexity: password
+Password: password
+Length: 8
+Has Uppercase: False
+Has Lowercase: True
+Has Digit: False
+Has Special Character: False
+Strength: Weak
+Feedback: Your password is weak. Password should include at least one uppercase letter. Password should include at least one number. Password should include at least one special character (e.g., !, @, #, $, etc.).
